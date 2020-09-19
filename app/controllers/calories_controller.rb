c31f2d38f0a929216385f254b54427fa71c502c7
Fyamada1229 <yamadafutosi1229@gmail.com>
2020-09-20 00:59:26 +0900

    add

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -1,11 +1,18 @@
 class CaloriesController < ApplicationController
 
   def index
-    
+    @calories = Calorie.all
   end
 
   def new
-    
+    @calorie = Calorie.new
+  end
+  
+  def create
+    Calorie.create(calorie_params)
   end
 
+  def name
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'calories#index'
-  resources :calories, only: [:index, :new]
+  resources :calories, only: [:index, :new, :create]
+  get 'name', to: 'calories#name'
 
 end
