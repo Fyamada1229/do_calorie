9638222c839769222f312e574daa1ab5bdadf217
Fyamada1229 <yamadafutosi1229@gmail.com>
2020-10-03 12:55:39 +0900

    edit add

--- app/controllers/calories_controller.rb
+++ app/controllers/calories_controller.rb
@@ -27,6 +27,12 @@ class CaloriesController < ApplicationController
     @calorie = Calorie.find(params[:id])
   end
 
+  def update
+    calorie = Calorie.find(params[:id])
+    calorie.update(calorie_params)
+    redirect_to root_path
+  end
+
   private
   def calorie_params
     params.require(:calorie).permit(:name, :content, :calorie_morning, :calorie_lunch, :calorie_night)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'calories#index'
-  resources :calories,  only: [:index, :new, :create, :show, :edit, :destroy]
+  resources :calories,  only: [:index, :new, :create, :show, :edit, :destroy, :update]
 
 end
