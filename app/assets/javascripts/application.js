69abece1c7c4cc67f10a3835a85a31909ec26756
Fyamada1229 <yamadafutosi1229@gmail.com>
2020-10-17 19:41:38 +0900

    add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,8 @@
 //= require rails-ujs 
 //= require turbolinks 
 //=require_tree .
+//= require chartkick
+//= require Chart.bundle
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'calories#index'
   resources :calories,  only: [:index, :new, :create, :show, :edit, :destroy, :update]
-
 end
