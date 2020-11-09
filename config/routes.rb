ffe78ecc1f0c005b1089d54f3470bbbb6aa2401f
Fyamada1229 <yamadafutosi1229@gmail.com>
2020-11-09 23:08:02 +0900

    routes add

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'calories#index'
   resources :calories,  only: [:index, :new, :create, :show, :edit, :destroy, :update]
+  resources :users, only: :show
 end
