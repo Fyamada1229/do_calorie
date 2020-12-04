27d7c10be368d1d1ee3f9619af8489c2111a5bd9
Fyamada1229 <yamadafutosi1229@gmail.com>
2020-12-04 14:17:47 +0900

    add

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,37 +1,91 @@
 'use strict';
 
-// // 自動日付計算
-// $(document).on('turbolinks:load', function() {
-//   $(function(){
-//     let date = new Date();
-//     let year = date.getFullYear();
-//     let month = date.getMonth() + 1;
-//     let day = date.getDate();
-//     let week_japanese = ["日", "月", "火", "水", "木", "金", "土"];
-//     let week = week_japanese[date.getDay()];
-//     let hour = date.getHours();
-//     let minute = date.getMinutes();
-//     let second = date.getSeconds();
-//     $("#demo").text(`${year}年${month}月${day}日(${week})`);
-//     // 表示：2020年1月1日(水) 
-//     $("#year__month").text(`${year}年${month}月`);
-//   });
-// });
+console.clear();
 
 // カレンダー
 {
   const year = 2020;
   const month = 4; //5月
 
+  function getCalendarHead () {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      // 30
+      // 29,30
+      // 28,29,30
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }    
+    console.log(dates);
+  }
+
   function getCalendarBody() {
     const dates = []; // date: 日付, day: 曜日
     const lastDate = new Date(year, month + 1, 0).getDate();
 
     for (let i = 1; i <= lastDate; i++) {
-      dates.push(i);
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: false,
+      });
+      console.log(dates);
     }
+  }
 
+
+
+  function getCalendarTall() {
+    const dates = [];
+    const lastDay = new Date(year, month + 1, 0).getDay();
+
+    for (let i = 1; i < 7 - lastDay; i++) {
+      dates.push({
+        date: i,
+        isToday: false,
+        isDisabled: true,
+      }); 
+    }
     console.log(dates);
   }
-  getCalendarBody();
+
+  function createCalendar() {
+    const dates =[
+      ...getCalendarHead(),
+      ...getCalendarHead(),
+      ...getCalendarTall(),
+    ];
+
+    const weeks = [];
+    const weeksCount = dates.length / 7;
+
+    for (let i = 0; i < weeksCount; i++) {
+      weeks.push(dates.splice(0,7));
+    }
+
+    console.log(weeks);
+
+    weeks.forEach(week => {
+      const tr = document.createElement('tr');
+      week.forEach(date => {
+        const td = document.createElement('td');
+
+        td.textContent = date.date;
+        if (date.isToday) {
+          td.classList.add('today');
+        }
+        if (date.isDisabled) {
+          td.classList.add('disabled');
+        }
+
+        tr.appendChild(td);
+      });
+      document.querySelector('tbody').appendChild(tr);
+    });
 }
