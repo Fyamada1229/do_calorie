377bb7de4c4e285ad38e4f962d5936f4b90667ee
Fyamada1229 <yamadafutosi1229@gmail.com>
2020-12-17 00:03:24 +0900

    add

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,6 @@ set :application, 'do_calorie'
 set :repo_url,  'git@github.com:Fyamada1229/do_calorie.git'
 
 
-
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
