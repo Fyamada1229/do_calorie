afd4a7b75493bc80cf6d88d256dc972e162cea58
Fyamada1229 <yamadafutosi1229@gmail.com>
2020-12-16 22:19:20 +0900

    add

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,5 @@ set :keep_releases, 5
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:stop'
-    invoke 'unicorn:start'
+    invoke 'unicorn:restart'
   end
